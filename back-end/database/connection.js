b669dabd25d70b404cdc92852eb462cbccc4b9a2
Vinicius do Canto <viniciustst7@gmail.com>
2026-04-28 21:51:13 -0300

    Versao final com SSL e tratativa de nome

--- back-end/database/connection.js
+++ back-end/database/connection.js
@@ -1,8 +1,8 @@
 const knex = require('knex');
 const configuration = require('../knexfile');
 
-// Esta lógica detecta se o sistema está no Render (production) ou no seu PC
-// Se a variável NODE_ENV for 'production', ele usa o banco do Supabase
+// Esta lógica detecta automaticamente se o sistema está no Render (production) ou no seu PC
+// No Render, a variável NODE_ENV vale 'production', ativando o banco do Supabase com SSL.
 const config = process.env.NODE_ENV === 'production' 
     ? configuration.production 
     : configuration.development;
