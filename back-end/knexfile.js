72732a3fe33eb22ab5815e0f19e61b79ce086d82
Vinicius do Canto <viniciustst7@gmail.com>
2026-04-28 22:00:09 -0300

    mais um teste

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    // Se existir DATABASE_URL, usa ela. Se não, usa os campos separados (local)
     connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
@@ -18,16 +17,14 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false } // ESSENCIAL para o Supabase no Render
-    },
-    migrations: {
-      directory: './database/migrations'
-    },
+    // Passamos a URL diretamente para evitar o erro de 'URL Inválida'
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
+    },
+    migrations: {
+      directory: './database/migrations'
     }
   }
 };
